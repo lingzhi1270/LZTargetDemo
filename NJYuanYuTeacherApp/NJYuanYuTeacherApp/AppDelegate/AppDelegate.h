3c7bcbb113375d2eac8102b9b88911b2ed18e307
凌志 <1534649018@qq.com>
2018-12-10 10:30:11 +0800

    工程提交

---
+++ NJYuanYuTeacherApp/NJYuanYuTeacherApp/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NJYuanYuTeacherApp
+//
+//  Created by 凌志 on 2018/12/10.
+//  Copyright © 2018年 lingzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
